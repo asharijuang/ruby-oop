132187e2656b87a4f5257214c50ffc564baeba92
Ashari Juang <asharijuang@me.com>
2014-12-09 18:01:41 +0700

    scoping

--- oop1.rb
+++ oop1.rb
@@ -22,3 +22,48 @@ javascript = Bahasa.new("JavaScript", "Brendan Eich")
 ruby.deskripsi
 python.deskripsi
 javascript.deskripsi
+
+# ------------------ Contoh class variable dan global variable ---------------------
+class MyClass
+  # variable dengan @@ hanya dapat dipanggil di dalam class
+  @@var_class = "ini class variable"
+  # variable dengan $ dapat dipanggil langsung dari luar class
+  $my_variable = "Hello!"
+end
+
+# memanggil variable di dalam class
+puts $my_variable
+puts "memanggil variable di dalam class MyClass :#{$my_variable}"
+
+# ---------------------------------- End -------------------------------------------
+
+class Computer
+
+  $manufacturer = "Mango Computer, Inc."
+  @@files = {hello: "Hello, world!"}
+
+  def initialize(username, password)
+    @username = username
+    @password = password
+  end
+
+  def current_user
+    @username
+  end
+
+  def self.display_files
+    @@files
+  end
+end
+
+# Make a new Computer instance:
+hal = Computer.new("Dave", 12345)
+
+puts "Current user: #{hal.current_user}"
+# @username belongs to the hal instance.
+
+puts "Manufacturer: #{$manufacturer}"
+# $manufacturer is global! We can get it directly.
+
+puts "Files: #{Computer.display_files}"
+# @@files belongs to the Computer class.
